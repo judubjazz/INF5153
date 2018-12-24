0a23e0dfb62f8f0a0c38ba506ce58516ea419f80
judubjazz <julienguite3@hotmail.com>
2018-12-24 03:38:19 -0500

    ship.js refactoring to ES8

--- battleship/src/main/resources/static/js/battleship/ship.js
+++ battleship/src/main/resources/static/js/battleship/ship.js
@@ -1,38 +1,39 @@
 /* Initial values*/
-var idShip = 5;
-var carrier = '"stem":{"x":0,"y":0},"bow":{"x":4,"y":0},"size":5';
-var battleship = '"stem":{"x":0,"y":1},"bow":{"x":3,"y":1},"size":4';
-var cruiser = '"stem":{"x":0,"y":2},"bow":{"x":2,"y":2},"size":3';
-var submarine = '"stem":{"x":0,"y":3},"bow":{"x":2,"y":3},"size":3';
-var destroyer = '"stem":{"x":0,"y":4},"bow":{"x":1,"y":4},"size":2';
-
-var data = '{"difficulty":' + run() + ',' +
-  '"name":"battleship",' +
-  '"fleet":{"carrier":{' + carrier + '},' +
-  '"cruiser":{' + cruiser + '},' +
-  '"battleship":{' + battleship + '},' +
-  '"submarine":{' + submarine + '},' +
-  '"destroyer":{' + destroyer + '}}}';
-
-document.getElementById("data").value = data;
+let idShip = 5;
+const black = 'rgb(26, 26, 26)';
+const white = 'rgb(255, 255, 255)';
+const grey = 'rgb(242, 242, 242)';
+const redError = 'rgb(244, 67, 54)';
+const redShipChooser = 'rgb(219, 60, 48)';
+let data = {
+  difficulty: false,
+  name : 'battleship',
+  fleet: {
+    carrier: {stem: {x: 0, y: 0}, bow: {x: 0, y: 4}, size: 5},
+    destroyer: {stem: {x: 1, y: 0}, bow: {x: 1, y: 1}, size: 2},
+    battleship: {stem: {x: 2, y: 0}, bow: {x: 2, y: 3}, size: 3},
+    cruiser: {stem: {x: 3, y: 0}, bow: {x: 3, y: 2}, size: 3},
+    submarine: {stem: {x: 4, y: 0}, bow: {x: 4, y: 3}, size: 4}
+  },
+};
 
 /*all white background*/
 function whiteBackground() {
-  for (i = 0; i <= 9; i++) {
-    for (j = 0; j <= 9; j++) {
-      document.getElementById("" + [i] + "," + [j] + "").style.backgroundColor = "#ffffff";
+  for (let i = 0; i <= 9; i++) {
+    for (let j = 0; j <= 9; j++) {
+      document.getElementById("" + [i] + "," + [j] + "").style.backgroundColor = white;
     }
   }
 }
 
 /*grey background except black or red cases*/
 function greyBackground() {
-  for (i = 0; i <= 9; i++) {
-    for (j = 0; j <= 9; j++) {
-      if (document.getElementById("" + [i] + "," + [j] + "").style.backgroundColor != 'rgb(244, 67, 54)'
-        && document.getElementById("" + [i] + "," + [j] + "").style.backgroundColor != 'rgb(219, 60, 48)'
-        && document.getElementById("" + [i] + "," + [j] + "").style.backgroundColor != 'rgb(26, 26, 26)') {
-        document.getElementById("" + [i] + "," + [j] + "").style.backgroundColor = "#f2f2f2";
+  for (let i = 0; i <= 9; i++) {
+    for (let j = 0; j <= 9; j++) {
+      if (document.getElementById("" + [i] + "," + [j] + "").style.backgroundColor != redError
+        && document.getElementById("" + [i] + "," + [j] + "").style.backgroundColor != redShipChooser
+        && document.getElementById("" + [i] + "," + [j] + "").style.backgroundColor != black) {
+        document.getElementById("" + [i] + "," + [j] + "").style.backgroundColor = grey;
       }
     }
   }
@@ -40,18 +41,13 @@ function greyBackground() {
 
 /*grey background except black or red cases*/
 function shipOnGrid() {
-  for (i = 0; i <= 9; i++) {
-    for (j = 0; j <= 9; j++) {
-      if (document.getElementById("" + [i] + "," + [j] + "").style.backgroundColor != 'rgb(26, 26, 26)') {
-        document.getElementById("" + [i] + "," + [j] + "").style.backgroundColor = "#ffffff";
+  for (let i = 0; i <= 9; i++) {
+    for (let j = 0; j <= 9; j++) {
+      if (document.getElementById("" + [i] + "," + [j] + "").style.backgroundColor != black) {
+        document.getElementById("" + [i] + "," + [j] + "").style.backgroundColor = white;
       }
     }
   }
-}
-
-/*get difficulty value*/
-function run() {
-  return document.getElementById("difficulty").value;
 }
 
 /*verified if ships coordinates or not empty or null*/
@@ -61,19 +57,24 @@ function validateShipsCoord(str, id) {
   if (!str || 0 === str.length) {
     switch (id) {
       case 5:
-        carrier = '"stem":{"x":0,"y":0},"bow":{"x":4,"y":0},"size":5';
+        let carrier = {stem:{x:0,y:0},bow:{x:4,y:0},size:5};
+        fleet.carrier= carrier;
         break;
       case 4:
-        battleship = '"stem":{"x":0,"y":1},"bow":{"x":3,"y":1},"size":4';
+        let battleship = {stem:{x:0,y:1},bow:{x:3,y:1},size:4};
+        fleet.battleship = battleship;
         break;
       case 3:
-        cruiser = '"stem":{"x":0,"y":2},"bow":{"x":2,"y":2},"size":3';
+        let cruiser = {stem:{x:0,y:2},bow:{x:2,y:2},size:3};
+        fleet.cruiser = cruiser;
         break;
       case 2:
-        submarine = '"stem":{"x":0,"y":3},"bow":{"x":2,"y":3},"size":3';
+        let submarine = {stem:{x:0,y:3},bow:{x:2,y:3},size:3};
+        fleet.submarine = submarine;
         break;
       case 1:
-        destroyer = '"stem":{"x":0,"y":4},"bow":{"x":1,"y":4},"size":2';
+        let destroyer = {stem:{x:0,y:4},bow:{x:1,y:4},size:2};
+        fleet.destroyer = destroyer;
         break;
     }
   }
@@ -84,129 +85,115 @@ whiteBackground();
 
 /*All ships*/
 function shipsChooser(coord) {
-  var tr = coord.parentNode.rowIndex;
-  var td = coord.cellIndex;
+  let tr = coord.parentNode.rowIndex;
+  let td = coord.cellIndex;
 
   if (idShip === 5) {
-    carrier = position("carrier", tr, td, 5);
+    data.fleet.carrier = position("carrier", tr, td, 5);
   }
   else if (idShip === 4) {
-    battleship = position("battleship", tr, td, 4);
+    data.fleet.battleship = position("battleship", tr, td, 4);
   }
   else if (idShip === 3) {
-    cruiser = position("cruiser", tr, td, 3);
+    data.fleet.cruiser = position("cruiser", tr, td, 3);
   }
   else if (idShip === 2) {
-    submarine = position("submarine", tr, td, 3);
+    data.fleet.submarine = position("submarine", tr, td, 3);
   }
   else if (idShip === 1) {
-    destroyer = position("destroyer", tr, td, 2);
+    data.fleet.destroyer = position("destroyer", tr, td, 2);
   }
-
-  data = '{"difficulty":' + run() + ',' +
-    '"name":"battleship",' +
-    '"fleet":{"carrier":{' + carrier + '},' +
-    '"cruiser":{' + cruiser + '},' +
-    '"battleship":{' + battleship + '},' +
-    '"submarine":{' + submarine + '},' +
-    '"destroyer":{' + destroyer + '}}}';
-
-  document.getElementById("data").value = data;
 }
 
 function start() {
-  data = '{"difficulty":' + run() + ',' +
-    '"name":"battleship",' +
-    '"fleet":{"carrier":{' + carrier + '},' +
-    '"cruiser":{' + cruiser + '},' +
-    '"battleship":{' + battleship + '},' +
-    '"submarine":{' + submarine + '},' +
-    '"destroyer":{' + destroyer + '}}}';
-
-  document.getElementById("data").value = data;
-  $('#submit-fleet-button').click();
+  if(idShip === 0){
+    let difficulty =  document.getElementById("difficulty").value;
+    data.difficulty = difficulty;
+    document.getElementById("data").value = JSON.stringify(data);
+    $('#submit-fleet-button').click();
+  }
 }
 
 
 function position(shipName, tr, td, size) {
 
-  var stemX, stemY, bowX, bowY;
-  var trR = 0;
-  var tdR = 0;
-  var validate = true;
+  let stemX, stemY, bowX, bowY;
+  let trR = 0;
+  let tdR = 0;
+  let validate = true;
 
-  if (document.getElementById(tr + "," + td).style.backgroundColor === 'rgb(255, 255, 255)') {
-    document.getElementById(tr + "," + td).style.backgroundColor = "#f44336";
+  if (document.getElementById(tr + "," + td).style.backgroundColor === white) {
+    document.getElementById(tr + "," + td).style.backgroundColor = redError;
     if ((tr + (size - 1)) < 10) {
-      for (i = tr; i <= tr + (size - 1); i++) {
-        if (document.getElementById(i + "," + td).style.backgroundColor === 'rgb(26, 26, 26)') {
+      for (let i = tr; i <= tr + (size - 1); i++) {
+        if (document.getElementById(i + "," + td).style.backgroundColor === black) {
           validate = false;
           break;
         }
       }
       if (validate === true) {
-        document.getElementById((tr + (size - 1)) + "," + td).style.backgroundColor = "#DB3C30";
+        document.getElementById((tr + (size - 1)) + "," + td).style.backgroundColor = redShipChooser;
       }
     }
     validate = true;
     if ((td + (size - 1)) < 10) {
-      for (i = td; i <= td + (size - 1); i++) {
-        if (document.getElementById(tr + "," + i).style.backgroundColor === 'rgb(26, 26, 26)') {
+      for (let i = td; i <= td + (size - 1); i++) {
+        if (document.getElementById(tr + "," + i).style.backgroundColor === black) {
           validate = false;
           break;
         }
       }
       if (validate === true) {
-        document.getElementById(tr + "," + (td + (size - 1))).style.backgroundColor = "#DB3C30";
+        document.getElementById(tr + "," + (td + (size - 1))).style.backgroundColor = redShipChooser;
       }
     }
     validate = true;
     if ((tr - (size - 1)) >= 0) {
-      for (i = tr; i >= tr - (size - 1); i--) {
-        if (document.getElementById(i + "," + td).style.backgroundColor === 'rgb(26, 26, 26)') {
-          validate = false
-          break;
-        }
-      }
-      if (validate === true) {
-        document.getElementById((tr - (size - 1)) + "," + td).style.backgroundColor = "#DB3C30";
-      }
-    }
-    validate = true;
-    if ((td - (size - 1)) >= 0) {
-      for (i = td; i >= td - (size - 1); i--) {
-        if (document.getElementById(tr + "," + i).style.backgroundColor === 'rgb(26, 26, 26)') {
+      for (let i = tr; i >= tr - (size - 1); i--) {
+        if (document.getElementById(i + "," + td).style.backgroundColor === black) {
           validate = false;
           break;
         }
       }
       if (validate === true) {
-        document.getElementById(tr + "," + (td - (size - 1))).style.backgroundColor = "#DB3C30";
+        document.getElementById((tr - (size - 1)) + "," + td).style.backgroundColor = redShipChooser;
+      }
+    }
+    validate = true;
+    if ((td - (size - 1)) >= 0) {
+      for (let i = td; i >= td - (size - 1); i--) {
+        if (document.getElementById(tr + "," + i).style.backgroundColor === black) {
+          validate = false;
+          break;
+        }
+      }
+      if (validate === true) {
+        document.getElementById(tr + "," + (td - (size - 1))).style.backgroundColor = redShipChooser;
       }
     }
     greyBackground();
   }
-  else if (document.getElementById(tr + "," + td).style.backgroundColor === 'rgb(244, 67, 54)') {
-    document.getElementById(tr + "," + td).style.backgroundColor = "#ffffff";
+  else if (document.getElementById(tr + "," + td).style.backgroundColor === redError) {
+    document.getElementById(tr + "," + td).style.backgroundColor = white;
     shipOnGrid();
   }
-  else if (document.getElementById(tr + "," + td).style.backgroundColor === 'rgb(219, 60, 48)') {
-    document.getElementById(tr + "," + td).style.backgroundColor = "#1a1a1a";
+  else if (document.getElementById(tr + "," + td).style.backgroundColor === redShipChooser) {
+    document.getElementById(tr + "," + td).style.backgroundColor = black;
 
-    for (i = 0; i <= 9; i++) {
-      for (j = 0; j <= 9; j++) {
-        if (document.getElementById("" + [i] + "," + [j] + "").style.backgroundColor === 'rgb(244, 67, 54)') {
+    for (let i = 0; i <= 9; i++) {
+      for (let j = 0; j <= 9; j++) {
+        if (document.getElementById("" + [i] + "," + [j] + "").style.backgroundColor === redError) {
           trR = i;
           tdR = j;
-          document.getElementById("" + [i] + "," + [j] + "").style.backgroundColor = "#1a1a1a";
+          document.getElementById("" + [i] + "," + [j] + "").style.backgroundColor = black;
           break;
         }
       }
     }
     if (trR === tr) {
       if (tdR < td) {
-        for (i = tdR; i <= td; i++) {
-          document.getElementById("" + [tr] + "," + [i] + "").style.backgroundColor = "#1a1a1a";
+        for (let i = tdR; i <= td; i++) {
+          document.getElementById("" + [tr] + "," + [i] + "").style.backgroundColor = black;
           if (i === tdR) {
             stemX = tr;
             stemY = i;
@@ -218,8 +205,8 @@ function position(shipName, tr, td, size) {
         }
       }
       if (td < tdR) {
-        for (i = td; i <= tdR; i++) {
-          document.getElementById("" + [tr] + "," + [i] + "").style.backgroundColor = "#1a1a1a";
+        for (let i = td; i <= tdR; i++) {
+          document.getElementById("" + [tr] + "," + [i] + "").style.backgroundColor = black;
           if (i === tdR) {
             bowX = tr;
             bowY = i;
@@ -233,8 +220,8 @@ function position(shipName, tr, td, size) {
     }
     if (tdR === td) {
       if (trR < tr) {
-        for (i = trR; i <= tr; i++) {
-          document.getElementById("" + [i] + "," + [td] + "").style.backgroundColor = "#1a1a1a";
+        for (let i = trR; i <= tr; i++) {
+          document.getElementById("" + [i] + "," + [td] + "").style.backgroundColor = black;
           if (i === trR) {
             stemX = i;
             stemY = td;
@@ -246,8 +233,8 @@ function position(shipName, tr, td, size) {
         }
       }
       if (tr < trR) {
-        for (i = tr; i <= trR; i++) {
-          document.getElementById("" + [i] + "," + [td] + "").style.backgroundColor = "#1a1a1a";
+        for (let i = tr; i <= trR; i++) {
+          document.getElementById("" + [i] + "," + [td] + "").style.backgroundColor = black;
           if (i === trR) {
             bowX = i;
             bowY = td;
@@ -260,9 +247,14 @@ function position(shipName, tr, td, size) {
       }
     }
     shipOnGrid();
-    idShip = idShip - 1;
+    --idShip;
   }
-  return '"stem":{"x":' + stemX + ',"y":' + stemY + '},"bow":{"x":' + bowX + ',"y":' + bowY + '},"size":' + size;
+  let ship = {
+    stem:{x:stemX, y:stemY},
+    bow:{x:bowX, y:bowY},
+    size,
+  };
+  return ship;
 }
 	
 	
